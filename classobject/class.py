8125047b587d65d9d602df82474b988140732645
MOHAMMED ZAKI BHOJANI <51822177+mzb2599@users.noreply.github.com>
2020-02-23 16:06:15 +0530

    Add files via upload

---
+++ classobject/class.py
@@ -0,0 +1,7 @@
+class Dog:
+    kind = 'Bull dog'
+    
+d = Dog()
+print(d.kind)
+d.kind = 'German Shepherd'
+print(d.kind)
---
+++ classobject/class1.py
@@ -0,0 +1,10 @@
+class Dog:
+    kind = 'Bull dog'
+    def __init__(self,name):
+        print('Instance created of type dog')
+        self.name=name
+d = Dog('Trump')
+print(d.kind)
+d.kind = 'German Shepherd'
+print('Type :',d.kind)
+print("Name :",d.name)
