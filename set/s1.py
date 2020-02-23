e1ed9e56c7b89464ddc3eae6bc95622f2689431a
MOHAMMED ZAKI BHOJANI <51822177+mzb2599@users.noreply.github.com>
2020-02-23 16:10:21 +0530

    Add files via upload

---
+++ set/s1.py
@@ -0,0 +1,12 @@
+num = {1, 2, 3, 4, 5}
+print(num)
+print(2 in num)
+print(5 in num)
+a = set('Mohd')
+b = set('Zaki')
+c=a-b
+print(c)
+c = a|b
+print(c)
+c = a&b
+print(c)
---
+++ set/s2.py
@@ -0,0 +1,13 @@
+#12-1-2020
+squares = {1, 4, 9, 16}
+print(squares)
+print(4 in squares)
+print(9 in squares)
+# set = squares - set(16)
+print(squares)
+# squares = squares + 16
+# i=5
+# for i in range(10):
+#     squares = squares + (i ** 2)
+#     i=i+1
+# print(squares)
