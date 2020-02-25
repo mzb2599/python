a672d365d643804059908b9656eedfb0e66fe342
MOHAMMED ZAKI BHOJANI <51822177+mzb2599@users.noreply.github.com>
2020-02-25 19:07:51 +0530

    Add files via upload

--- list/list.py
+++ list/list.py
@@ -1,11 +1,37 @@
-names=[]
-print(names)
-names.append('Zaki')
-print(names)
-names.append('Mohammed')
-print(names)
-names.append(110)
-names.append(7.9)
-print(names)
-del (names)
-print(names)
+List = [8, 'Mohd Zaki', 7.9, "Sem-4"]
+count = List.count(8)
+print(count)
+List.append(118888)
+print(List)
+list1=[5,1,7,9,3,2]
+list1.sort()
+print(list1)
+list1.reverse()
+print(list1)
+list1.remove(list1[1])
+print(list1)
+list1.extend(List)
+print(list1)
+List.clear()
+print(List)
+del (List)
+length = len(list1)
+print(length)
+list2 = [8, 'MZB', 'SEM-4', 'Python', 7.9]
+print(list2[3])
+print(list2[0:3])
+print(list1[-6:-2])
+#print(List)
+'''
+1
+[8, 'Mohd Zaki', 7.9, 'Sem-4', 118888]
+[1, 2, 3, 5, 7, 9]
+[9, 7, 5, 3, 2, 1]
+[9, 5, 3, 2, 1]
+[9, 5, 3, 2, 1, 8, 'Mohd Zaki', 7.9, 'Sem-4', 118888]
+[]
+10
+Python
+[8, 'MZB', 'SEM-4']
+[1, 8, 'Mohd Zaki', 7.9]
+'''
