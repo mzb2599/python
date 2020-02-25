a06f16dedc2a57358b7a99f3d027b55cd5712826
MOHAMMED ZAKI BHOJANI <51822177+mzb2599@users.noreply.github.com>
2020-02-25 19:07:15 +0530

    Add files via upload

---
+++ tuple/tuple.py
@@ -0,0 +1,26 @@
+Tuple = (3118008,'Mohd Zaki',7.9,123,'MHSS')
+print(Tuple)
+Tuple1 = ('MZB', 'MHSS', 123, 14.0)
+print(Tuple1)
+T = Tuple + Tuple1
+print(T)
+colour = tuple('Colour')
+print(colour)
+t = (5, 1, 7, 9, 3)
+print(len(t))
+print(max(t))
+print(min(t))
+print(sorted(t))
+print(sum(t))
+'''
+C:\Users\MOHD ZAKI\Desktop\python\college>py tuple.py
+(3118008, 'Mohd Zaki', 7.9, 123, 'MHSS')
+('MZB', 'MHSS', 123, 14.0)
+(3118008, 'Mohd Zaki', 7.9, 123, 'MHSS', 'MZB', 'MHSS', 123, 14.0)
+('C', 'o', 'l', 'o', 'u', 'r')
+5
+9
+1
+[1, 3, 5, 7, 9]
+25
+''' 
